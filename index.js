3b49ebc78773e1b4d5eea886d6a178467db5b6ae
annkim7@github.com <ilc7748@gmail.com>
2022-07-06 11:56:45 +0900

    1-3

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+//1-2
+// let 이름 :string = 'kim';
+// let 어레이 :string[] = ['kim', 'park'];
+// let 오브젝트 :{ name : string } = { name : 'kim' };
+// let 여러가지 :string | number = 'kim';
+// type Name = string | number;
+// let 이이름 :Name = 123;
+// function 함수(x :number) :number{
+//     return x * 2
+// }
+// type Member = [number, boolean];
+// let john:Member = [123, true];
+// type Member1 = {
+//     [ key :string ] : string,
+// }
+// let jane : Member1 = { name : 'kim'}
+// class User{
+//     name :string;
+//     constructor(name :string){
+//         this.name = name;
+//     }
+// }
+//1-3
+var 회원들 = ['kim', 'park'];
+var 회원들2 = { member1: 'kim', member2: 'park' };
+var 회원들3 = [1, 2, 3];
+//과제
 var 이름 = 'kim';
-var 어레이 = ['kim', 'park'];
-var 오브젝트 = { name: 'kim' };
-var 여러가지 = 'kim';
-var 이이름 = 123;
-function 함수(x) {
-    return x * 2;
-}
-var john = [123, true];
-var jane = { name: 'kim' };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+var 나이 = 20;
+var 지역 = '서울';
+var 노래 = { title: 'a', artist: 'b' };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,27 +1,50 @@
+//1-2
+// let 이름 :string = 'kim';
+// let 어레이 :string[] = ['kim', 'park'];
+// let 오브젝트 :{ name : string } = { name : 'kim' };
+// let 여러가지 :string | number = 'kim';
+
+// type Name = string | number;
+// let 이이름 :Name = 123;
+
+// function 함수(x :number) :number{
+//     return x * 2
+// }
+
+// type Member = [number, boolean];
+// let john:Member = [123, true];
+
+// type Member1 = {
+//     [ key :string ] : string,
+// }
+
+// let jane : Member1 = { name : 'kim'}
+
+// class User{
+//     name :string;
+//     constructor(name :string){
+//         this.name = name;
+//     }
+// }
+
+//1-3
+let 회원들 :string[] = ['kim', 'park'];
+let 회원들2 :{ member1 : string, member2 : string } = { member1 : 'kim', member2 : 'park' };
+let 회원들3 = [1,2,3];
+
+//과제
 let 이름 :string = 'kim';
-let 어레이 :string[] = ['kim', 'park'];
-let 오브젝트 :{ name : string } = { name : 'kim' };
-let 여러가지 :string | number = 'kim';
+let 나이 :number = 20;
+let 지역 :string = '서울';
 
-type Name = string | number;
-let 이이름 :Name = 123;
+let 노래 :{title : string, artist : string} = { title : 'a', artist : 'b'}
 
-function 함수(x :number) :number{
-    return x * 2
-}
-
-type Member = [number, boolean];
-let john:Member = [123, true];
-
-type Member1 = {
-    [ key :string ] : string,
-}
-
-let jane : Member1 = { name : 'kim'}
-
-class User{
-    name :string;
-    constructor(name :string){
-        this.name = name;
-    }
+let project :{
+    member : string[],
+    days : number,
+    started : boolean
+} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
 }
