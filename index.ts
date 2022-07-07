dedd29c3a5d0352dd912a91d9f25f16736312c31
annkim7@github.com <ilc7748@gmail.com>
2022-07-07 18:32:12 +0900

    1-4

--- index.ts
+++ index.ts
@@ -9,3 +9,33 @@ let 이름 :any;
 이름 = [];
 
 let 이름1 :unknown;
+이름1 = 123;
+이름 = {};
+
+let 변수1 :string = 이름;
+
+// let 나이 :string|number;
+// 나이+1; 안됨
+
+// let 나이 :unknown = 1;
+// 나이-1; 안됨
+
+//과제1
+let user :string = 'kim';
+let age :undefined | number = undefined;
+let married :boolean = false; 
+let 철수 :(string|number|undefined|boolean)[] = [user, age, married];
+
+//과제2
+let 학교 : {
+    score : (number|boolean)[],
+    teacher : string,
+    friend : string|string[]
+}
+= {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
