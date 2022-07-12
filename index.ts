02d5878339d8d27f0c5e181d26cdb1bdc414e2b1
annkim7@github.com <ilc7748@gmail.com>
2022-07-12 18:05:38 +0900

    1-9

--- index.ts
+++ index.ts
@@ -1,25 +1,9 @@
-//1-8
-let 이름 :123;
-이름 = 123;
+//1-9
 
-let 접니다:'대머리' | '솔로';
-접니다 = '대머리'
+type 함수타입 = (a :string) => number;
 
-function 함수(a :'hello') :1 | 0{
-    return 1
-}
+() => { return 10 }
 
-//과제
-function 함수1(a :'가위' | '바위' | '보') :('가위' | '바위' | '보')[]{
-    return ['가위']
-}
-
-var 자료 = {
-    name : 'kim'
-} as const
-
-function 내함수(a :'kim'){
+function 함수(){
 
 }
-
-내함수(자료.name)
